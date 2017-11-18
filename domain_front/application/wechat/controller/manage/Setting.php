dfa595cebba130605241e72867131e597b0e9aef
Oliver Wang <whtaok@163.com>
2017-11-18 22:47:34 +0800

    888

--- domain_front/application/wechat/controller/manage/Setting.php
+++ domain_front/application/wechat/controller/manage/Setting.php
@@ -16,6 +16,7 @@ namespace app\wechat\controller\manage;
 
 use function GuzzleHttp\json_encode;
 use app\wechat\model\WechatSetting;
+use function GuzzleHttp\delete;
 
 class Setting extends WechatController
 {
@@ -68,68 +69,12 @@ class Setting extends WechatController
     }
 
     /**
-     * 编辑课程
-     */
-    public function edit()
-    {
-        $params = $this->request->param();
-        if ($this->request->isPost()) {
-            $validate = new CourseValid();
-            $checkRst = $validate->check($params, '', 'edit');
-            if ($checkRst !== true) {
-                // 验证失败
-                $this->error($validate->getError());
-            } else {
-                // 验证通过
-                
-                //上传banner
-                $rtnBanner = $this->uploadCut('banner');
-                if($rtnBanner !== false){
-                    $params['banner'] = $rtnBanner;
-                }
-                
-                if ($params['id']) {
-                    // 更新操作
-                    $detail = GuankeCourse::manage()->find($params['id']);
-                    $flag = $detail->save($params);
-                    if ($flag !== false) {
-                        $this->success('保存成功', $this->getLastUrl());
-                    } else {
-                        $this->error('保存失败');
-                    }
-                } else {
-                    // 新增操作
-                    $params['cid'] = $this->getCid();
-                    $flag = GuankeCourse::create($params);
-                    if ($flag !== false) {
-                        $this->success('新增成功', $this->getLastUrl());
-                    } else {
-                        $this->error('新增失败');
-                    }
-                }
-            }
-        } else {
-            if ($params['id']) {
-                $detail = GuankeCourse::manage()->find($params['id']);
-                $this->assign('detail', $detail);
-            } else {
-                $detail = [
-                    'isdisplay' => 1
-                ];
-                $this->assign('detail', $detail);
-            }
-            
-            return $this->fetch('edit');
-        }
-    }
-
-    /**
      * 删除课程
      */
     public function remove()
     {
         $params = $this->request->param();
-        $flag = GuankeCourse::manage()->delete($params['id']);
+        $flag = WechatSetting::delete($this->getCid(),true);
         if ($flag == 1) {
             return $this->success('删除成功');
         } else {
@@ -138,30 +83,12 @@ class Setting extends WechatController
     }
 
     /**
-     * 排序变更
-     */
-    public function sortChange()
-    {
-        $id = $this->request->param('id');
-        $sort = $this->request->param('sort');
-        $detail = GuankeCourse::manage()->find($id);
-        $detail->sort = $sort;
-        $flag = $detail->save();
-        if ($flag !== false) {
-            $this->success('操作成功', '', $sort);
-        } else {
-            $this->error('操作失败', '', $sort);
-        }
-    }
-
-    /**
      * 状态变更
      */
     public function statusChange()
     {
-        $id = $this->request->param('id');
         $field = $this->request->param('field');
-        $detail = GuankeCourse::manage()->find($id);
+        $detail = WechatSetting::find($this->getCid());
         
         if ($detail->$field === 1) {
             $detail->$field = 0;
