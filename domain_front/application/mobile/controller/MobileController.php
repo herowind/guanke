32b1d133b79d3ff7d575098086862775ef2bd2a9
Oliver Wang <whtaok@163.com>
2017-11-20 00:37:48 +0800

    5555

--- domain_front/application/mobile/controller/MobileController.php
+++ domain_front/application/mobile/controller/MobileController.php
@@ -72,8 +72,8 @@ class MobileController extends MobileBaseController{
 	    		$this->initOfficialAccount();
 	    		if($this->officialAccount){
 	    			//微信授权开始
-	    			$oauth = $this->officialAccount->oauth;
-	    			$oauth->redirect();
+	    			$oauth = $this->officialAccount->oauth->scopes(['snsapi_userinfo']);
+	    			$oauth->redirect('http://guanke.qyhzlm.com/mobile/passport/loginwx.html');
 	    		}else{
 	    			//跳转至手机号登录页面
 	    		}
